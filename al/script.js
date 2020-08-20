69a5a935a4960f77f26588591aceed8ce7308332
kxmeng <46137783+kxmeng@users.noreply.github.com>
2020-08-20 19:04:26 +0100

    .

--- al/script.js
+++
@@ -1,7 +0,0 @@
-var shapestext = "<h2></h2><ul><li></li></ul>";
-
-$("#shapes").click(function() {
-	$('.active').removeClass("active");
-	$(this).addClass("active");
-	$("#content").html(shapestext);
-});
