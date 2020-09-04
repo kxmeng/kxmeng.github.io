631609db15e9353b11f87c3bdbe84e5c19f01e27
kxmeng <46137783+kxmeng@users.noreply.github.com>
2020-09-04 23:59:36 +0100

    old

---
+++ old/l5/specificationhigher.js
@@ -0,0 +1,321 @@
+var numbersIntegers = [
+	"understand and use integers (positive, negative and zero)",
+	"understand place value",
+	"use directed numbers in practical situations e.g. temperatures",
+	"order integers",
+	"use the four rules of addition, subtraction, multiplication and division",
+	"use brackets and the hierarchy of operations",
+	"use the terms 'odd', 'even', 'prime numbers', 'factors' and 'multiples'",
+	"identify prime factors, common factors and common multiples"
+];
+var numbersFractions = [
+	"understand and use equivalent fractions, simplifying a fraction by cancelling common factors",
+	"understand and use mixed numbers and vulgar fractions",
+	"order fractions and calculate a given fraction of a given quantity",
+	"express a given number as a fraction of another number",
+	"use common denominators to add and subtract fractions and mixed numbers",
+	"convert a fraction to a decimal or a percentage",
+	"understand and use unit fractions as multiplicative inverses",
+	"multiply and divide fractions and mixed numbers"
+];
+var numbersDecimals = [
+	"use decimal notation",
+	"understand place value",
+	"order decimals",
+	"convert a decimal to a fraction or a percentage",
+	"recognise that a terminating decimal is a fraction"
+];
+var numbersPowers = [
+	"identify square numbers and cube numbers",
+	"calculate squares, square roots, cubes and cube roots",
+	"use index notation and index laws for multiplication and division of positive and negative integer powers including zero",
+	"express integers as a product of powers of prime factors",
+	"find highest common factors and lowest common multiples",
+	"understand the meaning of surds",
+	"manipulate surds, including rationalising a denominator"
+	"use index laws to simplify and evaluate numerical expressions involving integer, fractional and negative powers"
+];
+var numbersSets = [
+	"understand the definition of a set",
+	"use the set notation \(\cup\), \(\cap\), \(\in\), \(\not\in\)",
+	"understand the concept of the universal set and the empty set and the symbols for these sets",
+	"understand and use the complement of a set",
+	"use Venn diagrams to represent sets",
+	"understand sets defined in algebraic terms, and understand and use subsets",
+	"use Venn diagrams to represent sets and the number of elements in sets",
+	"use the notation \(n(A)\) for the number of elements in the set \(A\)",
+	"use sets in practical situations"
+];
+var numbersPercentages = [
+	"undertand the percentage means number of parts per 100",
+	"express a given number as a percentage of another number",
+	"express a percentage as a fraction and as a decimal",
+	"understand the multiplicative nature of percentages as operators",
+	"solve simple percentage problems, including percentage increase and decrease",
+	"use reverse percentages",
+	"use compound interest and depreciation",
+	"use repeated percentage change",
+	"solve compound interest problems"
+];
+var numbersRatio = [
+	"use ratio notation, including reduction to its simplest form and its various links to fraction notation",
+	"divide a quantity in a given ratio or ratios",
+	"use the process of proportionality to evaluate unknown quantities",
+	"calculate an unknown quantity from quantities that vary in direct proportion",
+	"solve word problems about ratio and proportion"
+];
+var numbersAccuracy = [
+	"round integers to a given power of 10",
+	"round to a given number of significant figures or decimal places",
+	"identify upper and lower bounds where values are given to a degree of accuracy",
+	"use estimation to evaluate approximations to numerical calculations",
+	"solve problems using upper and lower bounds where values are given to a degree of accuracy"
+];
+var numbersStandardForm = [
+	"calculate with and interpret numbers in standard form",
+	"solve problems involving standard form"
+];
+var numbersApplications = [
+	"use and apply number in everyday person, domestic or community life",
+	"carry out calculations using standard (metric) units of mass, length, area, volume and capacity",
+	"understand and carry out calculations using time, and carry out calculations using money, including converting between currencies"
+];
+var numbersCalculators = [
+	"use a scientific electronic calculator to determine numerical results"
+];
+var numbers = [numbersIntegers,numbersFractions,numbersDecimals,numbersPowers,numbersSets,numbersPercentages,numbersRatio,numbersAccuracy,numbersStandardForm,numbersApplications,numbersCalculators];
+
+var equationsSymbols = [
+	"understand that symbols may be used to represent numbers in equations or variables in expressions and formulae",
+	"understand that algebraic expressions follow the generalised rules of arithmetic",
+	"use index notation for integer and fractional powers",
+	"use index laws in simple cases"
+];
+var equationsAlgebra = [
+	"evaluate expressions by substituting numerical values for letters",
+	"collect like terms",
+	"multiply a single term over a bracket",
+	"take out common factors",
+	"expand the product of two simple linear expressions",
+	"understand the concept of a quadratic expression and be able to factorise such expressions",
+	"expand the product of two or more linear expressions",
+	"manipulate algebraic fractions where the numerator and/or the denominator can be numeric, linear or quadratic",
+	"complete the square for a given quadratic expression",
+	"use algebra to support and construct proofs"
+];
+var equationsExpressions = [
+	"understand that a letter may represent an unknown number or a variable",
+	"use correct notational conventions for algebraic expressions and formulae",
+	"substitute positive and negative integers, decimals and fractions for words and letters in expressions and formulae",
+	"use formulae from mathematics and other real life contexts expressed initially in words or diagrammatic form and convert to letters and symbols",
+	"derive a formula or expression",
+	"understand the process of manipulating formulae or equations to change the subject, to include cases where the subject may appear twice or a power of the subject occurs"
+];
+var equationsLinear = [
+	"solve linear equations, with integer or fractional coefficients, in one unknown",
+	"set up simple linear equations from given data"
+];
+var equationsProportion = [
+	"set up problems involving direct or inverse proportion and relate algebraic solutions to graphical representation of these equations",
+];
+var equationsSimultaneous = [
+	"calculate the exact solution of two simultaneous equations in two unknowns",
+	"interpret the equations as lines and the common solution as the point of intersection"
+];
+var equationsQuadratic = [
+	"solve quadratic equations by factorisation",
+	"solve quadratic equations by using the formula or completing the square",
+	"form and solve quadratic equations from data given in a context",
+	"solve simultaneous equations in. two unknowns, one being linear and the other being quadratic"
+];
+var equationsInequalities = [
+	"understand and use the symbols \(>\), \(<\), \(\geq\) and \(\leq\)",
+	"understand and use the convention for open and closed intervals on a number line",
+	"solve simple linear inequalities in one variable and represent the solution set on a number line",
+	"represent simple linear inequalities on rectangular Cartesian graphs",
+	"identify regions on rectangular Cartesian graphs defined by simple linear inequalities",
+	"solve quadratic inequalities in one unknown and represent the solution set on a number line",
+	"identify harder examples of regions defined by liner inequalities"
+];
+var equations = [equationsSymbols,equationsAlgebra,equationsExpressions,equationsLinear,equationsProportion,equationsSimultaneous,equationsQuadratic,equationsInequalities];
+
+var functionsSequences = [
+	"generate terms of a sequence using term to term and position to term definitions of the sequence",
+	"find subsequent terms of an integer sequence and the rule for generating it",
+	"use linear expressions to describe the nth term of arithmetic sequences",
+	"understand and use common difference and first term in an arithmetic sequence",
+	"know and use the nth term = \(a + (n-1)d\)",
+	"find the sum of the first n terms of an arithmetic series"
+];
+var functionsNotation = [
+	"understand the concept that a function is a mapping between elements of two sets",
+	"use function notations of the form \(f(x)\) and \(f : x \mapsto\)",
+	"understand the terms 'domain' and 'range' and which values may need to be excluded from a domain",
+	"understand and find the composite function \(fg\) and the inverse function \(f^{-1}\)"
+];
+var functionsGraphs = [
+	"interpret information presented in a range of linear and non-linear graphs",
+	"understand and use concentions for rectangular Cartesian coordinates",
+	"plot points \((x,y)\) in any of the four quadrants or locate points with given coordinates",
+	"determine the coordinates of points identified by geometrical information",
+	"determine the coordinates of the midpoint of a line segment, given the coordinates of the two end points",
+	"draw and interpret straight line conversion graphs",
+	"find the gradient of a straight line",
+	"recognise that equations of the form \(y = mx + c\) are straight line graphs with gradient \(m\) and intercept on the y-axis \((0,c)\)",
+	"recognise, generate points and plot graphs of linear and quadratic functions",
+	"recognise, plot and draw cubic graphs where the letters \(x\) and \(y\) can be replaced with any two other letters, graphs with equation \(y = Ax^3 + Bx^2 + Cx + D + \frac{E}{x} + \frac{F}{x^2}\) in which at least three of the constants are zero and the letters \(x\) and \(y\) can be replaced with any two other letters, graphs with equation \(y = \sin x, \cos x, \tan x\) for angles of any size",
+	"apply to the graph of \(y = f(x)\) the transformations \(y = f(x)+a, f(ax), f(x+a), af(x)\) for linear, quadratic, sine and cosine functions",
+	"interpret and analyse transformations of functions and write the functions algebraically",
+	"find the gradients of non-linear graphs by drawing a tangent",
+	"find the intersection points of two graphs, one linear and one non-linear, and recognise that the solutions correspond to the solutions of \(y_2-y_1=0\)",
+	"calculate the gradient of a straight line given the coordinates of two points",
+	"find the equation of a straight line parallel or perpendicular to a given line"
+];
+var functionsCalculus = [
+	"understand the concept of a variable rate of change",
+	"differentiate integer powers of \(x\)",
+	"determine gradients, rates of change, stationary points, turning points by differentiation and relate these to graphs",
+	"distinguish between maxima and minima by considering the general shape of the graph only",
+	"apply calculus to linear kinematics and to other simple practical problems"
+];
+var functions = [functionsSequences,functionsNotation,functionsGraphs,functionsCalculus];
+
+var shapesAngles = [
+	"distinguish between acute, obtuse, reflex and right angles",
+	"use angle properties of intersecting lines, parallel lines and angles on a straight line",
+	"understand the exterior angle of a triangle property and the angle sum of a triangle property",
+	"understand the terms 'isosceles', 'equilateral' and 'right-angled triangles' and the angle properties of these triangles"
+];
+var shapesPolygons = [
+	"recognise and give the names of: parallelogram, rectangle, square, rhombus, trapezium, kite, pentagon, hexagon and octagon",
+	"understand and use the term 'quadrilateral' and the angle sum property of quadrilaterals",
+	"understand and use the properties of the parallelogram, rectangle, square, rhombus, trapezium and kite",
+	"understand the term 'regular polygon' and calculate interior and exterior angles of regular polygons",
+	"understand and use the angle sum of polygons",
+	"understand congruence as meaning the same shape and size",
+	"understand that two or more polygons with the same shape and size are said to be congruent to each other"
+];
+var shapesSymmetry = [
+	"identify any lines of symmetry and the order of rotational symmetry of a given two dimensional figure"
+];
+var shapesMeasures = [
+	"interpret scales on a range of measuring instruments",
+	"calculate time intervals in terms of the 24 hour and the 12 hour clock",
+	"make sensible estimates of a range of measures",
+	"understand angle measure including three figure bearings",
+	"measure an angle to the nearest degree",
+	"understand and use the relationship between average speed, distance and time",
+	"use compound measure such as speed, density and pressure"
+];
+var shapesConstruction = [
+	"measure and draw lines to the nearest millimetre",
+	"construct triangles and other two dimensional shapes using a combination of a ruler, protractor and compass",
+	"solve problems using scale drawings",
+	"use straight edge and compasses to construct the perpendicular bisector of a line segment and the bisector of an angle"
+];
+var shapesCircles = [
+	"recognise the terms 'centre', 'radius', 'chord', 'diameter', 'circumference', 'tangent', 'arc', 'sector' and 'segment' of a circle",
+	"understand chord and tangent properties of circles",
+	"understand and use the internal and external intersecting chord properties",
+	"recognise the term 'cyclic quadrilateral'",
+	"understand and use angle properties of the circle: angle subtended by an arc at the centre is twice the angle at the circumference; angle subtended at the circumference by a diameter is a right angle; angles in the same segment are equal; opposite angles in a cyclic quadrilateral sum to 180 degrees; the alternate segment theorem",
+];
+var shapesReasoning = [
+	"provide reasons, using standard geometrical statements, to support numerical values for angles obtained in any geometrical context involving lines, polygons and circles"
+];
+var shapesTrigonometry = [
+	"know, understand and use Pythagoras' theorem in two and three dimensions",
+	"know, understand and use sine, cosine and tangent of acute angles to determine lengths and angles of a right angled triangle",
+	"apply trigonometrical methods to solve problems in two dimensions",
+	"understand and use sine, cosine and tangent of obtuse angles",
+	"understand and use angles of elevation and depression",
+	"understand and use the sine and cosine rules for any triangle",
+	"understand and use the formula for area of a triangle",
+	"apply trigonometrical methods to solve problems in three dimensions, including finding the angle between a line and a plane"
+];
+var shapesMensuration = [
+	"convert measurements within the metric system to include linear and area units",
+	"find the perimeter of shapes made from triangles and rectangles",
+	"find the area of simple shapes using the formulae for the areas of triangles and rectangles",
+	"find the area of parallelograms and trapezia",
+	"find circumferences and areas of circles; find perimeters and areas of sectors of circles"
+];
+var shapes3d = [
+	"recognise and give names of: cube, cuboid, prism, pyramid, cylinder, sphere, cone",
+	"understand the terms 'face', 'edge' and 'vertex' in the context of 3D solids",
+	"find the surface area of simple shapes using the area formulae for triangles and rectangles",
+	"find the surface area of a cylinder",
+	"find the volume of prisms, including cuboids and cylinders, using an appropriate formula",
+	"convert between units of volume within the metric system",
+	"find the surface area and volume of a sphere and a right circular cone using relevant formulae"
+];
+var shapesSimilarity = [
+	"understand and use the geometrical properties that similar figures have corresponding lengths in the same ratio but corresponding angles remain unchanged",
+	"use and interpret maps and scale drawings",
+	"understand that areas of similar figures are in the ratio of the square of corresponding sides",
+	"understand that volumes of similar figures are in the ratio of the cube of corresponding sides",
+	"use areas and volumes of similar figures in solving problems"
+];
+var shapes = [shapesAngles,shapesPolygons,shapesSymmetry,shapesMeasures,shapesConstruction,shapesCircles,shapesReasoning,shapesTrigonometry,shapesMensuration,shapes3d,shapesSimilarity];
+
+var vectorsVectors = [
+	"understand that  a  vector has both magnitude and direction",
+	"understand and use vector notation including column vectors",
+	"multiply vectors by scalar quantities",
+	"add and subtract vectors",
+	"calculate the modulus of a vector",
+	"find the resultant of two or more vectors",
+	"apply vector methods for simple geometrical proofs"
+];
+var vectorsTransformations = [
+	"understand that rotations are specified by a centre and an angle",
+	"rotate a shape about a point through a given angle",
+	"recognise that an anti-clockwise rotation is a positive angle of rotation and a clockwise rotation is a negative angle of rotation",
+	"understand that reflections are specified by a mirror line",
+	"construct a mirror line given an object and reflect a shape given a mirror line",
+	"understand that translations are specified by a distance and a direction",
+	"translate a shape",
+	"understand and use column vectors in translations",
+	"understand that rotations, reflections and translations preserve length and angle so that a transformed shape under any of these transformations remains congruent to the original shape",
+	"understand that enlargements are specified by a centre and a scale factor",
+	"understand that enlargements preserve angles and not lengths",
+	"enlarge a shape given the scale factor",
+	"identify and give complete descriptions of transformations"
+];
+var vectors = [vectorsVectors,vectorsTransformations];
+
+var dataGraphs = [
+	"use different methods of presenting data: pictograms, bar charts and pie charts, and two-way tables",
+	"use appropriate methods of tabulation to enable the contructino of statistical diagrams",
+	"interpret statistical diagrams",
+	"construct and interpret histograms",
+	"construct cumulative frequency diagrams from tabulated data",
+	"use cumulative frequency diagrams"
+];
+var dataStats = [
+	"understand the concept of average",
+	"calculate the mean, median, mode and range for a discrete data set",
+	"calculate an estimate for the mean for grouped data",
+	"identify the modal class for grouped data",
+	"estimate the median from a cumulative frequency diagram",
+	"understand the concept of a measure of spread",
+	"find the interquartile range from a discrete set of data",
+	"estimate the interquartile range from a cumulative frequency diagram"
+];
+var dataProbability = [
+	"understand the language of probability: outcomes, equal likelihood, events, random",
+	"understand and use the probability scale",
+	"understand and use estimates or measures of probability from theoretical models",
+	"find probabilities from a Venn diagram",
+	"understand the concepts of a sample space and an event, and how the probability of an event can be determined from the sample space",
+	"list all the outcomes for single events and for two successive events in a systematic way",
+	"estimate probabilities from previously collected data",
+	"calculate the probability of the complement of an event happening",
+	"use the addition rule of probability for mutually exclusive events",
+	"understand and use the term 'expected frequency'",
+	"draw and use tree diagrams",
+	"determine the probability that two or more independent events will occur",
+	"use simple conditional probability when combining events",
+	"apply probability to simple problems"
+];
+var data = [dataGraphs,dataStats,dataProbability];
