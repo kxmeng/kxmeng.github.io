0b144a314a71a421993498a7e913487e3ac1e31e
lusmaths <74881307+lusmaths@users.noreply.github.com>
2021-09-12 17:29:27 +0100

    Delete popup.js

--- isams/popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("button1").addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["script.js"],
-    });
-  });
